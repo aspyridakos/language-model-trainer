b7a8ee8259feb7c0591cc9f9ef51ed205794821d
Alexandra <aspyridakos@gmail.com>
2023-11-26 14:16:17 -0500

    started task 3

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# language-model-trainer
-Uses Word2Vec to find closest synonyms of words and their provided synonym options
+# Word2Vec Language Model Trainer
+Uses Word2Vec to find the closest synonyms of words and their provided synonym options
 
 ## Contributors
-// TODO: Add missing IDs
 - Alexandra Spyridakos 40175280
 - Christos Karabatsos 40132730
 - Oumar Barry 40172989
+
+## Run instructions
+- Uncomment line 11 on first run to install remainder of ntlk packages
+
+```python
+python main.py
+```
+
+### Sources
+Book files have been installed from [gutenberg.org](https://www.gutenberg.org/), a free and open source digital library of literature no longer subject to copyright laws. 
--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-import json
-import random
-from gensim import downloader
 import csv
+import json
+import os
+import random
+from gensim import downloader, models
 import matplotlib.pyplot as plt
+import nltk
+
+
+# TODO: Uncomment me for first run only
+# nltk.download('punkt')
+
+
 def get_closest_synonym(word_data, wv):
     """
     Get the closest synonym of a word from a list of word choices.
@@ -17,16 +25,21 @@ def get_closest_synonym(word_data, wv):
     closest_choice = max(similarities, key=similarities.get, default=None)
     return (closest_choice, "correct") if closest_choice == word_data['answer'] else (closest_choice, "wrong")
 
-def evaluate_model(model_name, dataset):
+
+def evaluate_model(model_name, dataset, corpus=None):
     """
     Run the synonym task with a given model and dataset, then return analysis results.
     """
-    wv = downloader.load(model_name)
+    if corpus:
+        wv = downloader.load(corpus)
+    else:
+        wv = downloader.load(model_name)
     correct_count = 0
     non_guess_count = 0
 
     with open(f'{model_name}-details.csv', 'w', newline='') as csv_file:
-        csv_writer = csv.DictWriter(csv_file, delimiter=',', fieldnames=["Question Word", "Answer Word", "Guess Word", "Evaluation Type"])
+        csv_writer = csv.DictWriter(csv_file, delimiter=',',
+                                    fieldnames=["Question Word", "Answer Word", "Guess Word", "Evaluation Type"])
         csv_writer.writeheader()
 
         for word_data in dataset:
@@ -35,7 +48,8 @@ def evaluate_model(model_name, dataset):
                 correct_count += 1
             if status != "guess":
                 non_guess_count += 1
-            csv_writer.writerow({"Question Word": word_data['question'], "Answer Word": word_data['answer'], "Guess Word": closest_choice, "Evaluation Type": status})
+            csv_writer.writerow({"Question Word": word_data['question'], "Answer Word": word_data['answer'],
+                                 "Guess Word": closest_choice, "Evaluation Type": status})
 
     vocab_size = len(wv.key_to_index)
     accuracy = correct_count / non_guess_count if non_guess_count > 0 else 0
@@ -46,6 +60,8 @@ def evaluate_model(model_name, dataset):
         'non_guess_count': non_guess_count,
         'accuracy': accuracy
     }
+
+
 def plot_model_performance(results, random_baseline_accuracy, human_gold_standard=None):
     """
     Plot the performance of models along with the random baseline and human gold-standard.
@@ -69,18 +85,53 @@ def plot_model_performance(results, random_baseline_accuracy, human_gold_standar
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.savefig('model_performance.png')
+
+
+def processed_books():
+    corpus_list = []
+    book_names = []
+
+    directory = './books'
+    for filename in os.listdir(directory):
+        if filename.endswith('.txt'):
+            with open(os.path.join(directory, filename), 'r', encoding='utf-8') as book_file:
+                text = book_file.read()
+                sentences = nltk.sent_tokenize(text, language='english')
+                params_list = [{"window_size": 2, "embedding_size": 10}, {"window_size": 20, "embedding_size": 5}]
+
+                for params in params_list:
+                    corpus = models.Word2Vec(sentences=sentences, window=params['window_size'],
+                                             vector_size=params['embedding_size'])
+
+                    book_name = f"{filename.rstrip('.txt')}_corpus-E{params['embedding_size']}-W{params['window_size']}"
+                    book_names.append(book_name)
+
+                    corpus_name = f"{book_name}.model"
+                    corpus.save(corpus_name)
+                    corpus_list.append(corpus_name)
+
+    return book_names, corpus_list
+
+
 def main():
     # Load dataset
     with open('synonym.json', 'r') as file:
         dataset = json.load(file)
 
+    book_names, corpus_list = processed_books()
+
     # Model names list
-    model_names = ['word2vec-google-news-300', 'glove-wiki-gigaword-100', 'glove-twitter-100', 'glove-twitter-25', 'glove-twitter-50']
+    model_names = ['word2vec-google-news-300', 'glove-wiki-gigaword-100', 'glove-twitter-100', 'glove-twitter-25',
+                   'glove-twitter-50'] + book_names
 
     # Run models and gather results
     results = []
     for model_name in model_names:
-        results.append(evaluate_model(model_name, dataset))
+        corpus_index = model_names.index(model_name)
+        if corpus_index > 4:
+            results.append(evaluate_model(model_name, dataset, corpus_list[corpus_index - 4]))
+        else:
+            results.append(evaluate_model(model_name, dataset))
 
     # Write analysis.csv
     with open('analysis.csv', 'w', newline='') as analysis_file:
